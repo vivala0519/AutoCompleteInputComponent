cd7be485e30aa3c2e1b404da0c2d15956fc04131
Dzeko <42686784+vivala0519@users.noreply.github.com>
2022-05-28 10:37:02 +0900

    [feat] inputbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import generatedData from './generated.json';
 
 function App() {
+  const [inputs, setInputs] = useState('');
+  const [result, setResult] = useState<string[]>([]);
+
+  const inputFunction = (e: any) => {
+    setInputs(e.target.value);
+    setResult([]);
+    for (let i = 0; i < generatedData.length; i++) {
+      if (generatedData[i]['name'].includes(inputs)) {
+        setResult([...result, generatedData[i]['name']]);
+      }
+    }
+  };
+  const resultList = result.map(q => <a className="list-item">{q}</a>);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="control">
+        <input type="text" className="input" onChange={inputFunction} />
+      </div>
+      <div className="list is-hoverable">{resultList}</div>
     </div>
   );
 }
